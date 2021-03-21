9273785632df84fdac465c5a92f455be8be827fd
DiasSv <dima,sveshnikov.81@mail.ru>
2021-03-21 20:46:57 +0300

    Добавил пару комментов

--- test_main_page.py
+++ test_main_page.py
@@ -3,6 +3,7 @@ from pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
+    page = MainPage(browser, link) # передаем данные
+    page.open() # Открываем страницу
+    page.go_to_login_page() # переходим на стр логин
+    page.should_be_login_link() # проверяем есть ли ссылка на логин
