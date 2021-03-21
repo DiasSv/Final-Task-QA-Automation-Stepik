83d6c31fad5e27f01df34f8e16ab2e28d12e209c
DiasSv <dima,sveshnikov.81@mail.ru>
2021-03-21 13:49:03 +0300

    Добавил комментарий

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,6 @@
 class BasePage():
-
+    """Мы создаем конструктор, в котором передаются тело
+    браузера и ссылка для дальнейшего использования"""
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
