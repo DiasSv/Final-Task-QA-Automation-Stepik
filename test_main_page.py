53388fd6d03e009a53d20a4080cb30d040518930
DiasSv <dima,sveshnikov.81@mail.ru>
2021-03-21 22:54:03 +0300

    промоакция закончилась, и фичу с изменением шапки откатили назад. Теперь ссылка работает так же, как раньше. Удалите ссылку с промоакцией, и верните обычную ссылку для запуска тестов:
    link = "http://selenium1py.pythonanywhere.com/"

--- test_main_page.py
+++ test_main_page.py
@@ -2,8 +2,8 @@ from pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-    page = MainPage(browser, link) # передаем данные
-    page.open() # Открываем страницу
-    page.go_to_login_page() # переходим на стр логин
-    page.should_be_login_link() # проверяем есть ли ссылка на логин
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)  # передаем данные
+    page.open()  # Открываем страницу
+    page.go_to_login_page()  # переходим на стр логин
+    page.should_be_login_link()  # проверяем есть ли ссылка на логин
