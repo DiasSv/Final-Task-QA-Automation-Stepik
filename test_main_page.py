30f04b37c27657c5943396ffb56b705fb6a67f71
DiasSv <dima,sveshnikov.81@mail.ru>
2021-03-21 13:45:00 +0300

    Мы написали почти хороший тест, который использует паттерн page object Тест использует классы base и main в папке pages, использует конструктор итд

--- test_main_page.py
+++ test_main_page.py
@@ -1,17 +1,9 @@
-import time
+from pages.main_page import MainPage
 
 
-link = "http://selenium1py.pythonanywhere.com/"
+def test_guest_can_go_to_login_page(browser):
 
-
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
-    time.sleep(5)
-
-
-class MainPage:
-
-    def test_guest_can_go_to_login_page(self, browser):
-        browser.get(link)
-        go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
