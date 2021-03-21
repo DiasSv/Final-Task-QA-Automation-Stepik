32918918fbbf21b036a037549e7b10edf5d18723
DiasSv <dima,sveshnikov.81@mail.ru>
2021-03-21 22:46:58 +0300

    Добавил функцию поиска и поднятия ошибки

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,20 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage():
     """Мы создаем конструктор, в котором передаются тело
     браузера и ссылка для дальнейшего использования"""
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=5):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
 
     def open(self):
         self.browser.get(self.url)
