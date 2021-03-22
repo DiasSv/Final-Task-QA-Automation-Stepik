dea8b7c0012edd427af641ddf3059dd0d2ed49cb
DiasSv <dima,sveshnikov.81@mail.ru>
2021-03-22 18:55:50 +0300

    Добавление локаторов для тестирования старницы логина

--- pages/locators.py
+++ pages/locators.py
@@ -2,4 +2,10 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators():
-    LOGIN_LINK = (By.ID, "#login_link")
+    LOGIN_LINK = (By.ID, "login_link")
+
+
+class LoginPageLocators():
+    # login_url = (By.)
+    LOGIN_FORM = (By.ID, "login_form")
+    register_form = (By.ID, "register_form")
