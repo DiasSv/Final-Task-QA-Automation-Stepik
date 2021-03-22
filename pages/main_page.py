6cdbea66a4c81fe518d5394091797a67a7a9dcd7
DiasSv <dima,sveshnikov.81@mail.ru>
2021-03-22 19:03:03 +0300

    Написание первого способа перехода между страницами путем retutn

--- pages/main_page.py
+++ pages/main_page.py
@@ -9,5 +9,19 @@ class MainPage(BasePage):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
+        '''
+        Теперь в тесте нам не нужно думать про
+        инициализацию страницы: она уже создана. Сохранив возвращаемое значение в переменную, мы можем использовать
+        методы новой страницы в тесте 
+        
+        минусы: 
+        если у нас копится большое количество страниц и переходов — образуется много перекрестных импортов;
+        большая связность кода — при изменении логики придется менять возвращаемое значение;
+        сложнее понимать код, так как страница инициализируется неявно;
+        образуются циклические зависимости, что часто приводит к ошибкам.
+        '''
+
+        return LoginPage(browser=self.browser, url=self.browser.current_url)
+
     def should_be_login_link(self):  # проверяет наличие элемента на странице
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "login link is not present"
