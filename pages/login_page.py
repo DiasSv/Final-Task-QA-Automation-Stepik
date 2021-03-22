d02de419347f26d6f810ab45ba5d630d4a14b07e
DiasSv <dima,sveshnikov.81@mail.ru>
2021-03-22 18:56:41 +0300

    Напиши функцию проверки url, login form and register form

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -8,13 +9,10 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        assert "login" in self.browser.current_url, "URL LOGIN IS NOT FOUNDED"  # наличие строки "логин" в url страницы
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form not founded"  # наличие формы логина
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.register_form), "Register form not founded"
