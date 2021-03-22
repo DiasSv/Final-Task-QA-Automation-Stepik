cd786cc49e476c00a0b7fafb9412f30ed26ceebb
DiasSv <dima,sveshnikov.81@mail.ru>
2021-03-23 00:36:01 +0300

    Вызвал функции для получения кода и для проверки успешности добавления продукта

--- test_product_page.py
+++ test_product_page.py
@@ -5,4 +5,8 @@ def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     product_page = ProductPage(browser, link)
     product_page.open()
-    product_page.go_to_basket()
+    product_page.go_to_basket()  # Нажимаю на кнопку добавления в корзину
+    product_page.solve_quiz_and_get_code()  # Провека алерта для получения кода прохождения
+    product_page.should_be_product_in_alert()  # Проверяем действительно ли у нас добавился наш товар
+
+
