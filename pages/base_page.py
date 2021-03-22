a10e20c5fac81a63a0e6e45101f826cdaa1c2b25
DiasSv <dima,sveshnikov.81@mail.ru>
2021-03-23 00:36:49 +0300

    Маленькие помарки

--- pages/base_page.py
+++ pages/base_page.py
@@ -27,7 +27,7 @@ class BasePage():
         x = alert.text.split(" ")[2]
         answer = str(math.log(abs((12 * math.sin(float(x))))))
         alert.send_keys(answer)
-        alert.acept()
+        alert.acсept()
         try:
             alert = self.browser.switch_to.alert
             alert_text = alert.text
