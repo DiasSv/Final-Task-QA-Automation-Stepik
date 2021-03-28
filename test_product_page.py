d709039471e0a0e2044e162866db76382a341317
DiasSv <dima,sveshnikov.81@mail.ru>
2021-03-28 09:36:53 +0300

    Произведи проверку промоакций и найди баг
    Написаны данные параметризации
    Изменена немного строка с сылкой, для того ,чтобы применить параметры
    И внесены мелкие помарки в код, улучшаюшие его читабельность

--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,19 @@
+import pytest
+
 from pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+# For promo, finding a bug
+@pytest.mark.xfail
+@pytest.mark.parametrize('links', ["?promo=offer0", "?promo=offer1", "?promo=offer2",
+                                   "?promo=offer3", "?promo=offer4", "?promo=offer5",
+                                   "?promo=offer6", "?promo=offer7", "?promo=offer8",
+                                   "?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, links):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{links}"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.go_to_basket()  # Нажимаю на кнопку добавления в корзину
     product_page.solve_quiz_and_get_code()  # Провека алерта для получения кода прохождения
-    product_page.should_be_product_in_alert()  # Проверяем действительно ли у нас добавился наш товар
-    product_page.should_be_okey_price()
+    product_page.should_be_product_in_basket()  # Проверяем действительно ли у нас добавился наш товар
+    product_page.should_be_right_price()  # Проверяет соотношение цены в корзине и цены товара
