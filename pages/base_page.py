d5e1e27028bc1ba991cb3204d49262e780bc8cf7
DiasSv <dima,sveshnikov.81@mail.ru>
2021-03-29 14:30:55 +0300

    Изменения параметров в функции is_disappeared

--- pages/base_page.py
+++ pages/base_page.py
@@ -24,7 +24,7 @@ class BasePage():
     '''Эта функция отвечает за проверку отсутствия элемента на странице'''
     def is_disappeared(self, how, what, timeout=4):
         try:
-            WebDriverWait(self.browser, timeout).until_not(EC.presence_of_element_located((how, what)))
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).until_not(EC.presence_of_element_located((how, what)))
         except TimeoutException:
             return False
 
