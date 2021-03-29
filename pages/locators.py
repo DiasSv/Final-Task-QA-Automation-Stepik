ff459e343ab89688e7c804bee9ea5c97ffe5382a
DiasSv <dima,sveshnikov.81@mail.ru>
2021-03-29 18:39:31 +0300

    Добавлен локатор для успешного сообщения ,который выходит при добавлении товара в корзину

--- pages/locators.py
+++ pages/locators.py
@@ -16,3 +16,4 @@ class ProductPageLocators():
     name_product = (By.CSS_SELECTOR, "div.col-sm-6.product_main > h1")
     price = (By.CSS_SELECTOR, "div.col-sm-6.product_main > p.price_color")
     price_add_basket = (By.CSS_SELECTOR, "div.alert.alert-safe.alert-noicon.alert-info.fade.in > div.alertinner strong")
+    success_message_after_adding_product_to_basket = (By.CSS_SELECTOR, "#messages > div:first-child")
