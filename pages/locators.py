cd4051833419a61cf24cdab2398f300fa9810049
DiasSv <dima,sveshnikov.81@mail.ru>
2021-03-29 18:44:35 +0300

    В файле locators.py создаем новый класс BasePageLocators И добавляем туда два локатора ссылки логина

--- pages/locators.py
+++ pages/locators.py
@@ -17,3 +17,8 @@ class ProductPageLocators():
     price = (By.CSS_SELECTOR, "div.col-sm-6.product_main > p.price_color")
     price_add_basket = (By.CSS_SELECTOR, "div.alert.alert-safe.alert-noicon.alert-info.fade.in > div.alertinner strong")
     success_message_after_adding_product_to_basket = (By.CSS_SELECTOR, "#messages > div:first-child")
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
