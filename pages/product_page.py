1f710d974fa4a9d19a5e754a40799370df51a23b
DiasSv <dima,sveshnikov.81@mail.ru>
2021-03-29 18:40:40 +0300

    Напиши тело функции (Написаны два тела)
    1 should_not_be_success_message_after_adding_product_to_basket
    
    2 should_be_message_disappeared_after_adding_product_to_basket

--- pages/product_page.py
+++ pages/product_page.py
@@ -3,7 +3,7 @@ from .locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
-    def go_to_basket(self):
+    def add_product_to_basket(self):
         basket_link = self.browser.find_element(*ProductPageLocators.basket_link)
         basket_link.click()
 
@@ -14,3 +14,11 @@ class ProductPage(BasePage):
     def should_be_right_price(self):
         assert self.browser.find_element(*ProductPageLocators.price).text == \
                self.browser.find_element(*ProductPageLocators.price_add_basket).text, "Price is not okay"
+
+    def should_not_be_success_message_after_adding_product_to_basket(self):
+        assert self.is_not_element_present(*ProductPageLocators.success_message_after_adding_product_to_basket), \
+            "Success message is presented, but should not be"
+
+    def should_be_message_disappeared_after_adding_product_to_basket(self):
+        assert self.is_disappeared(*ProductPageLocators.success_message_after_adding_product_to_basket), \
+            "the message was not lost"
