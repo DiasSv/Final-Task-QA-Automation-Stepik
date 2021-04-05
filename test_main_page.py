ece843582c28c0c6a727926b0a9205ff804315ba
DiasSv <dima,sveshnikov.81@mail.ru>
2021-04-05 22:45:24 +0300

    напиши Пример группировки тестов в один класс и его маркировки, для дальнейшего использования

--- test_main_page.py
+++ test_main_page.py
@@ -22,3 +22,15 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page.go_to_basket_page()  # Переходим на страницу корзины
     page.should_be_no_item_in_the_basket()  # Проверяем нет ли в корзине товаров
     page.should_be_text_empty_basket()  # Проверяем, есть ли текст о том , что корзина пуста
+
+
+'''Пример группировки тестов в один класс и его маркировки, для дальнейшего использования'''
+
+
+@pytest.mark.login_guest
+class TestLoginMainPage():
+    def test_guest_can_go_to_login_page(self):
+        pass
+
+    def test_guest_should_see_login_link(self):
+        pass
